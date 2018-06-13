4a7358d9017a3464ca177051543dd74b2d23f7ae
Artem.H <art.pzt@gmail.com>
2018-06-13 22:07:58 +0300

    urls + view + base responce

--- test_pdflib/apps/pdfcreator/views.py
+++ test_pdflib/apps/pdfcreator/views.py
@@ -1,2 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import HttpResponse
+from django.contrib.auth.models import User
 
+def view(request):
+    # print('in view')
+    # print (request.GET)
+    if 'user_id' in  request.GET:
+        if request.GET.dict()['user_id'].isdigit():
+            print('Create pdf in here')
+            user = User.objects.get(id=request.GET.dict()['user_id'])
+            for i in user.answers.all():
+                print ('question:', i.question_name,' answer:', i.value)
+            responce = 'Your looking for user with id: ' + request.GET.dict()['user_id']
+            return HttpResponse(responce)
+        else:
+            return HttpResponse('Enter correct user ID. I do not know who to look for (enter after the address ?user_id=* where * is ID user in youd DB)')
+    else:
+        return HttpResponse('I do not know who to look for (enter after the address ?user_id=* where * is ID user in youd DB)')
--- test_pdflib/test_pdflib/urls.py
+++ test_pdflib/test_pdflib/urls.py
@@ -2,9 +2,10 @@
 from django.conf.urls import url
 from django.contrib import admin
 
-# from apps.pdfcreator.views import PdfView
+from apps.pdfcreator.views import view
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    # url(r'^responce/', PdfView.as_view),
+    url(r'^responce/', view),
+    #?user_id={}
 ]
